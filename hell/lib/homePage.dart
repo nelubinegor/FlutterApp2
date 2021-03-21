68a1e7ba17c9ea138d2001806170faa1380c99d1
nelubinegor <56864147+nelubinegor@users.noreply.github.com>
2021-03-21 21:02:55 +0100

    Calendar added

--- hell/lib/homePage.dart
+++ hell/lib/homePage.dart
@@ -10,8 +10,33 @@ class HomePage extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
       ),
-      body: Center(
-        child: Text('Calendar Here'),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 7,
+        // Generate 100 widgets that display their index in the List.
+
+        children: List.generate(31, (index) {
+          return Center(
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(6.0),
+              child: Center(
+                child: Text(
+                  '${index + 1}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                color: Colors.blueGrey,
+                shape: BoxShape.circle,
+              ),
+            ),
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
